b0de2e29e2580f0273aecf278c62344464a24c23
Yuya Hirayama <y@yura.me>
2017-12-11 20:30:31 +0900

    Add Xcode project

---
+++ EssenceInjection/EssenceInjection.h
@@ -0,0 +1,19 @@
+//
+//  EssenceInjection.h
+//  EssenceInjection
+//
+//  Created by Yuya Hirayama on 2017/12/11.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EssenceInjection.
+FOUNDATION_EXPORT double EssenceInjectionVersionNumber;
+
+//! Project version string for EssenceInjection.
+FOUNDATION_EXPORT const unsigned char EssenceInjectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssenceInjection/PublicHeader.h>
+
+
